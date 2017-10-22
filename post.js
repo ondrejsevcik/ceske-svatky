2d379e135c64d1dd3d16695d552eca2e55ae8eb1
Ondrej Sevcik <ondrej.sev@gmail.com>
2017-10-22 17:01:05 +0200

    Add ability to tweet correct date

--- post.js
+++ post.js
@@ -1,39 +1,31 @@
 var Twit = require('twit');
 var dotenv = require('dotenv');
 var fs = require('fs');
+var moment = require('moment-timezone');
+var jmeniny = require('./cs-jmeniny.json');
 
 // Setup environment variables
 dotenv.config();
 
 var twit = new Twit({
-  consumer_key: process.env.CONSUMER_KEY, 
+  consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   // optional HTTP request timeout to apply to all requests.
-  timeout_ms: 60*1000,  
+  timeout_ms: 60*1000,
 });
 
+var dayMonth = moment.tz('Europe/Prague').format('MM-DD')
+var jmena = jmeniny[dayMonth];
+if (jmena.length < 0) {
+  console.warn('Dneska nema nikdo svatek');
+}
 
-// post a tweet with media
-var b64content = fs.readFileSync('./julie.png', { encoding: 'base64' })
 
-// first we must post the media to Twitter
-twit.post('media/upload', { media_data: b64content }, function (err, data, response) {
-  // now we can assign alt text to the media, for use by screen readers and
-  // other text-based presentations and interpreters
-  var mediaIdStr = data.media_id_string
-  var altText = "Dnes ma svatek Julie";
-  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
 
-  twit.post('media/metadata/create', meta_params, function (err, data, response) {
-    if (!err) {
-      // now we can reference the media and post a tweet (media will attach to the tweet)
-      var params = { status: 'Dnes ma svatek Test!', media_ids: [mediaIdStr] }
-
-      twit.post('statuses/update', params, function (err, data, response) {
-        console.log(data)
-      })
-    } 
-  })
+var message = 'Dnes ma svátek ' + jmena.join(', ');
+twit.post('statuses/update', { status: message }, function(err, data, response) {
+  console.log(data)
 })
+
