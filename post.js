9cc6e352adae4f48b1660f5495bae53c1e266ade
Ondrej Sevcik <ondrej.sev@gmail.com>
2017-10-22 13:43:13 +0200

    Add image proof of concept

--- post.js
+++ post.js
@@ -1,5 +1,6 @@
 var Twit = require('twit');
 var dotenv = require('dotenv');
+var fs = require('fs');
 
 // Setup environment variables
 dotenv.config();
@@ -13,6 +14,26 @@ var twit = new Twit({
   timeout_ms: 60*1000,  
 });
 
-twit.post('statuses/update', { status: 'hello world! from the app' }, function(err, data, response) {
-  console.log(data)
+
+// post a tweet with media
+var b64content = fs.readFileSync('./julie.png', { encoding: 'base64' })
+
+// first we must post the media to Twitter
+twit.post('media/upload', { media_data: b64content }, function (err, data, response) {
+  // now we can assign alt text to the media, for use by screen readers and
+  // other text-based presentations and interpreters
+  var mediaIdStr = data.media_id_string
+  var altText = "Dnes ma svatek Julie";
+  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+  twit.post('media/metadata/create', meta_params, function (err, data, response) {
+    if (!err) {
+      // now we can reference the media and post a tweet (media will attach to the tweet)
+      var params = { status: 'Dnes ma svatek Test!', media_ids: [mediaIdStr] }
+
+      twit.post('statuses/update', params, function (err, data, response) {
+        console.log(data)
+      })
+    } 
+  })
 })
